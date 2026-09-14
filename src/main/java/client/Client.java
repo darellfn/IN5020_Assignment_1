6f42e1d5f157d879c91b124def66ca26bc327621
Darell <darellfn@uio.no>
2026-09-14 14:06:47 +0200

    Add client and server

---
+++ src/main/java/client/Client.java
@@ -0,0 +1,14 @@
+package client;
+
+import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import server.ServerInterface;
+
+public class Client {
+    
+}
---
+++ src/main/java/server/Server.java
@@ -0,0 +1,11 @@
+package server;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.AlreadyBoundException;
+
+public class Server {
+    
+}
