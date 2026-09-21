371eaed0c2dcdb44b19c5ebd2b5c582b30ee0a4d
iechambe <iechambe@ulrik.uio.no>
2026-09-21 22:45:03 +0200

    Proxy server and interface nearly completed (just missing return values for client requests)

--- src/main/java/com/ass1/proxy/ProxyInterface.java
+++ src/main/java/com/ass1/proxy/ProxyInterface.java
@@ -2,7 +2,15 @@ package com.ass1.proxy;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-public interface ProxyInterface extends Remote{
-    int Add(int num1,int num2) throws RemoteException;
-    
+
+public interface ProxyInterface extends Remote {
+
+    // for clients
+
+    void requestServer(int zone) throws RemoteException;
+
+    // for servers
+
+    void registerNewServer(String ip, int port, String name) throws RemoteException;
+
 }
