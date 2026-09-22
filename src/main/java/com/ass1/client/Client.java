87aa6818a4b90b9b9d25be9d6a49d80f21991738
Darell <darellfn@uio.no>
2026-09-22 11:44:10 +0200

    update/Connected all parts

--- src/main/java/com/ass1/client/Client.java
+++ src/main/java/com/ass1/client/Client.java
@@ -6,24 +6,26 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-// import com.ass1.proxy.ProxyInterface;
-// import com.ass1.server.ServerInterface;
+import com.ass1.proxy.ProxyInterface;
+import com.ass1.server.ServerInterface;
 
 public class Client {
+    private final int T = 50;
 
-    public static void main(String[] args) throws RemoteException, NotBoundException {
-        parseQuery("exercise_1_input.txt");
+    public static void main(String[] args) throws RemoteException, NotBoundException, InterruptedException {
+        Client client = new Client();
+        client.parseQuery("exercise_1_input.txt");
 
-        System.out.println("IT WORKS YOOHOO!!!");
     }
 
-    public static void parseQuery(String fileName) throws RemoteException, NotBoundException {
+    public void parseQuery(String fileName) throws RemoteException, NotBoundException, InterruptedException {
         File file = new File(fileName);
 
-        // Registry proxyRegistry = LocateRegistry.getRegistry("localhost", 1099);
-        // ProxyInterface proxy = (ProxyInterface) proxyRegistry.lookup("Proxy");
+        Registry proxyRegistry = LocateRegistry.getRegistry(1099);
+        ProxyInterface proxy = (ProxyInterface) proxyRegistry.lookup("proxy");
     
         try (Scanner scanner = new Scanner(file);) {
             while(scanner.hasNextLine()) {
@@ -36,44 +38,20 @@ public class Client {
                         String[] zoneLine = line[3].split(":");
                         int zoneNumber = Integer.parseInt(zoneLine[1]);
 
-
-                        // String address = proxy.getAddress(zoneNumber);
-                        // int port = proxy.getPort(zoneNumber);
-                        // System.out.println(port);
-                        // System.out.println(address);
-                        // Registry serverRegistry = LocateRegistry.getRegistry(address, port);
-                        // ServerInterface serverStub = (ServerInterface) serverRegistry.lookup(proxy.getName(zoneNumber));
-                        // System.out.println(serverStub.getPopulationofCountry(country));
-                        //System.out.println("Method: " + method + " country: " + country + " Zone: " + zoneNumber);
+                        runPopulationOfCountry(proxy, country, zoneNumber);   
 
                     } else if (line.length < 3) { //I added this condition because some lines do not include any country e.g. "getPopulationofCountry Zone:3"
                         String[] zoneLine = line[1].split(":");
                         int zoneNumber = Integer.parseInt(zoneLine[1]);
 
-                        // String address = proxy.getAddress(zoneNumber);
-                        // int port = proxy.getPort(zoneNumber);
-                        // System.out.println(port);
-                        // System.out.println(address);
-                        // Registry serverRegistry = LocateRegistry.getRegistry(address, port);
-                        // ServerInterface serverStub = (ServerInterface) serverRegistry.lookup(proxy.getName(zoneNumber));
-                        // System.out.println(serverStub.getPopulationofCountry(""));
-                        
-                        //System.out.println("Method: " + method + " Zone: " + zoneNumber);
+                        runPopulationOfCountry(proxy, "", zoneNumber);                        
 
                     } else {
                         String country = line[1];
                         String[] zoneLine = line[2].split(":");
                         int zoneNumber = Integer.parseInt(zoneLine[1]);
 
-                        // String address = proxy.getAddress(zoneNumber);
-                        // int port = proxy.getPort(zoneNumber);
-                        // System.out.println(port);
-                        // System.out.println(address);
-                        // Registry serverRegistry = LocateRegistry.getRegistry(address, port);
-                        // ServerInterface serverStub = (ServerInterface) serverRegistry.lookup(proxy.getName(zoneNumber));
-                        // System.out.println(serverStub.getPopulationofCountry(country));
-                        
-                        //System.out.println("Method: " + method + " country: " + country + " Zone: " + zoneNumber);
+                        runPopulationOfCountry(proxy, country, zoneNumber);   
                     }
                     
                 } else if (method.equals("getNumberofCities")) {
@@ -84,31 +62,15 @@ public class Client {
                         String[] zoneLine = line[5].split(":");
                         int zoneNumber = Integer.parseInt(zoneLine[1]);
 
-                        // String address = proxy.getAddress(zoneNumber);
-                        // int port = proxy.getPort(zoneNumber);
-                        // System.out.println(port);
-                        // System.out.println(address);
-                        // Registry serverRegistry = LocateRegistry.getRegistry(address, port);
-                        // ServerInterface serverStub = (ServerInterface) serverRegistry.lookup(proxy.getName(zoneNumber));
-                        // System.out.println(serverStub.getNumberofCities(country, threshold, comp));
-
-                        //System.out.println("Method: " + method + " country: " + country + " Threshold: " + threshold + " Comp: " + comp + " Zone: " + zoneNumber);
+                        runNumberOfCities(proxy, country, threshold, comp, zoneNumber);
 
                     } else if (line.length < 5) {
                         int threshold = Integer.parseInt(line[1]);
                         String comp = line[2];
                         String[] zoneLine = line[3].split(":");
                         int zoneNumber = Integer.parseInt(zoneLine[1]);
-
-                        // String address = proxy.getAddress(zoneNumber);
-                        // int port = proxy.getPort(zoneNumber);
-                        // System.out.println(port);
-                        // System.out.println(address);
-                        // Registry serverRegistry = LocateRegistry.getRegistry(address, port);
-                        // ServerInterface serverStub = (ServerInterface) serverRegistry.lookup(proxy.getName(zoneNumber));
-                        // System.out.println(serverStub.getNumberofCities("", threshold, comp));
-
-                        //System.out.println("Method: " + method + " Threshold: " + threshold + " Comp: " + comp + " Zone: " + zoneNumber);
+                        
+                        runNumberOfCities(proxy, "", threshold, comp, zoneNumber);
 
                     } else {
                         String country = line[1];
@@ -117,15 +79,8 @@ public class Client {
                         String[] zoneLine = line[4].split(":");
                         int zoneNumber = Integer.parseInt(zoneLine[1]);
 
-                        // String address = proxy.getAddress(zoneNumber);
-                        // int port = proxy.getPort(zoneNumber);
-                        // System.out.println(port);
-                        // System.out.println(address);
-                        // Registry serverRegistry = LocateRegistry.getRegistry(address, port);
-                        // ServerInterface serverStub = (ServerInterface) serverRegistry.lookup(proxy.getName(zoneNumber));
-                        // System.out.println(serverStub.getNumberofCities(country, threshold, comp));
+                        runNumberOfCities(proxy, country, threshold, comp, zoneNumber);
 
-                        //System.out.println("Method: " + method + " country: " + country + " Threshold: " + threshold + " Comp: " + comp + " Zone: " + zoneNumber);
                     }
                 } else if (method.equals("getNumberofCountries")) {
                     int cityCount = Integer.parseInt(line[1]);
@@ -134,15 +89,8 @@ public class Client {
                     String[] zoneLine = line[4].split(":");
                     int zoneNumber = Integer.parseInt(zoneLine[1]);
 
-                    // String address = proxy.getAddress(zoneNumber);
-                    // int port = proxy.getPort(zoneNumber);
-                    // System.out.println(port);
-                    // System.out.println(address);
-                    // Registry serverRegistry = LocateRegistry.getRegistry(address, port);
-                    // ServerInterface serverStub = (ServerInterface) serverRegistry.lookup(proxy.getName(zoneNumber));
-                    // System.out.println(serverStub.getNumberofCountries(cityCount, threshold, comp));
+                    runNumberOfCountries(proxy, cityCount, threshold, comp, zoneNumber);
 
-                    //System.out.println("Method: " + method + " City Count: " + cityCount + " Threshold: " + threshold + " Comp: " + comp + " Zone: " + zoneNumber);
                 } else {
                     int cityCount = Integer.parseInt(line[1]);
                     int minPopulation = Integer.parseInt(line[2]);
@@ -150,21 +98,120 @@ public class Client {
                     String[] zoneLine = line[4].split(":");
                     int zoneNumber = Integer.parseInt(zoneLine[1]);
 
-                    // String address = proxy.getAddress(zoneNumber);
-                    // int port = proxy.getPort(zoneNumber);
-                    // System.out.println(port);
-                    // System.out.println(address);
-                    // Registry serverRegistry = LocateRegistry.getRegistry(address, port);
-                    // ServerInterface serverStub = (ServerInterface) serverRegistry.lookup(proxy.getName(zoneNumber));
-                    // System.out.println(serverStub.getNumberofCountriesMM(cityCount, minPopulation, maxPopulation));
-                    
-                    //System.out.println("Method: " + method + " City Count: " + cityCount + " Min population: " + minPopulation + " Max population: " + maxPopulation + " Zone: " + zoneNumber);
+                    runNumberOfCountriesMM(proxy, cityCount, minPopulation, maxPopulation, zoneNumber);
                 }
+                Thread.sleep(T);
             }
 
         } catch (FileNotFoundException e) {
             System.err.println(e);
 
         }
+    }
+
+    private void runPopulationOfCountry(ProxyInterface proxy, String country, int zone) {
+        Thread thread = new Thread(() -> {
+
+            try {
+                long start = System.currentTimeMillis();
+
+                ArrayList<String> serverInfo = proxy.requestServer(zone);
+                int port = Integer.parseInt(serverInfo.get(1));
+                String serverName = serverInfo.get(2);
+                Registry serverRegistry = LocateRegistry.getRegistry(port);
+                ServerInterface serverStub = (ServerInterface) serverRegistry.lookup(serverName);
+                int result = serverStub.getPopulationofCountry(country);
+
+                long end = System.currentTimeMillis();
+
+                System.out.println("Result: " + result + " | Time: " + (end - start) + " ms");
+
+            } catch (RemoteException | NotBoundException e) {
+                System.out.println("Error");
+            }
+
+            
+        });
+
+        thread.start();
+    }
+
+    private void runNumberOfCities(ProxyInterface proxy, String country, int threshold, String comp, int zone) {
+        Thread thread = new Thread(() -> {
+
+            try {
+                long start = System.currentTimeMillis();
+
+                ArrayList<String> serverInfo = proxy.requestServer(zone);
+                int port = Integer.parseInt(serverInfo.get(1));
+                String serverName = serverInfo.get(2);
+                Registry serverRegistry = LocateRegistry.getRegistry(port);
+                ServerInterface serverStub = (ServerInterface) serverRegistry.lookup(serverName);
+                int result = serverStub.getNumberofCities(country, threshold, comp);
+
+                long end = System.currentTimeMillis();
+
+                System.out.println("Result: " + result + " | Time: " + (end - start) + " ms");
+
+            } catch (RemoteException | NotBoundException e) {
+                System.out.println("Error");
+            }
+
+            
+        });
+
+        thread.start();
+
+    }
+
+    private void runNumberOfCountries(ProxyInterface proxy, int cityCount, int threshold, String comp, int zone) {
+        Thread thread = new Thread(() -> {
+
+            try {
+                long start = System.currentTimeMillis();
+
+                ArrayList<String> serverInfo = proxy.requestServer(zone);
+                int port = Integer.parseInt(serverInfo.get(1));
+                String serverName = serverInfo.get(2);
+                Registry serverRegistry = LocateRegistry.getRegistry(port);
+                ServerInterface serverStub = (ServerInterface) serverRegistry.lookup(serverName);
+                int result = serverStub.getNumberofCountries(cityCount, threshold, comp);
+
+                long end = System.currentTimeMillis();
+
+                System.out.println("Result: " + result + " | Time: " + (end - start) + " ms");
+
+            } catch (RemoteException | NotBoundException e) {
+                System.out.println("Error");
+            }
+        });
+
+        thread.start();
+
+    }
+
+    private void runNumberOfCountriesMM(ProxyInterface proxy, int cityCount, int minPopulation, int maxPopulation, int zone) {
+        Thread thread = new Thread(() -> {
+
+            try {
+                long start = System.currentTimeMillis();
+
+                ArrayList<String> serverInfo = proxy.requestServer(zone);
+                int port = Integer.parseInt(serverInfo.get(1));
+                String serverName = serverInfo.get(2);
+                Registry serverRegistry = LocateRegistry.getRegistry(port);
+                ServerInterface serverStub = (ServerInterface) serverRegistry.lookup(serverName);
+                int result = serverStub.getNumberofCountriesMM(cityCount, minPopulation, maxPopulation);
+
+                long end = System.currentTimeMillis();
+
+                System.out.println("Result: " + result + " | Time: " + (end - start) + " ms");
+
+            } catch (RemoteException | NotBoundException e) {
+                System.out.println("Error");
+            }
+        });
+
+        thread.start();
     }
 }
--- src/main/java/com/ass1/server/ServerSimulator.java
+++ src/main/java/com/ass1/server/ServerSimulator.java
@@ -1,22 +1,34 @@
 package com.ass1.server;
 
+import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
-import java.rmi.AlreadyBoundException;
+
+import com.ass1.proxy.ProxyInterface;
 
 public class ServerSimulator{
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws NotBoundException {
 
         try{
+            Registry registry = LocateRegistry.getRegistry(1099); // Må finne ut API til serverproxy 
+            ProxyInterface proxy = (ProxyInterface) registry.lookup("proxy");
+
+            int port = 5002;
+
             for(int i=0; i< 5; i++){
-                Registry registry = LocateRegistry.getRegistry(); // Må finne ut API til serverproxy 
                 Server server = new Server();
+                Registry serverRegistry = LocateRegistry.createRegistry(port);
                 ServerInterface serverStub = (ServerInterface) UnicastRemoteObject.exportObject(server, 0);
-                registry.bind("server" + i, serverStub);
+                serverRegistry.bind("server" + i, serverStub);
+                proxy.registerNewServer("localhost", port, "server" + i);
+                port++;
             }
+
+            System.out.println("Server is running...");
         }catch(RemoteException | AlreadyBoundException e){
             e.printStackTrace();
         }
