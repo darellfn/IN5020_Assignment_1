21015d72a8648bcb8a30d249e6b4a1734318c092
Darell <darellfn@uio.no>
2026-09-24 02:36:40 +0200

    Client file revision

--- src/main/java/com/ass1/client/Client.java
+++ src/main/java/com/ass1/client/Client.java
@@ -133,7 +133,7 @@ public class Client {
             }
         }
 
-        if (outputFile.equals("client_cache.txt") && !clientCache) {
+        if (outputFile.equals("output/client_cache.txt") && !clientCache) {
             System.err.println("Error: cannot write to client cache file when client cache is disabled. Please enable client cache.");
             System.exit(1);
         }
@@ -166,8 +166,7 @@ public class Client {
                         String country = line[1] + " " + line[2];
                         String[] zoneLine = line[3].split(":");
                         int zoneNumber = Integer.parseInt(zoneLine[1]);
-
-                        runPopulationOfCountry(proxy, country, zoneNumber, writer, query, method);   
+                        runQuery(proxy, country, 0, 0, 0, 0, "", zoneNumber, writer, query, method);   
 
                     } else if (line.length < 3) { //I added this condition because some query are missing a country e.g. "getPopulationofCountry <missing country here> Zone:3"
                         continue;                     
@@ -176,8 +175,7 @@ public class Client {
                         String country = line[1];
                         String[] zoneLine = line[2].split(":");
                         int zoneNumber = Integer.parseInt(zoneLine[1]);
-
-                        runPopulationOfCountry(proxy, country, zoneNumber, writer, query, method);   
+                        runQuery(proxy, country, 0, 0, 0, 0, "", zoneNumber, writer, query, method);   
                     }
                     
                 } else if (method.equals("getNumberofCities")) {
@@ -187,8 +185,7 @@ public class Client {
                         String comp = line[4];
                         String[] zoneLine = line[5].split(":");
                         int zoneNumber = Integer.parseInt(zoneLine[1]);
-
-                        runNumberOfCities(proxy, country, threshold, comp, zoneNumber, writer, query, method);
+                        runQuery(proxy, country, threshold, 0, 0, 0, comp, zoneNumber, writer, query, method);
 
                     } else if (line.length < 5) { //Missing country on query
                         continue;
@@ -199,18 +196,16 @@ public class Client {
                         String comp = line[3];
                         String[] zoneLine = line[4].split(":");
                         int zoneNumber = Integer.parseInt(zoneLine[1]);
-
-                        runNumberOfCities(proxy, country, threshold, comp, zoneNumber, writer, query, method);
-
+                        runQuery(proxy, country, threshold, 0, 0, 0, comp, zoneNumber, writer, query, method);
                     }
+
                 } else if (method.equals("getNumberofCountries")) {
                     int cityCount = Integer.parseInt(line[1]);
                     int threshold = Integer.parseInt(line[2]);
                     String comp = line[3];
                     String[] zoneLine = line[4].split(":");
                     int zoneNumber = Integer.parseInt(zoneLine[1]);
-
-                    runNumberOfCountries(proxy, cityCount, threshold, comp, zoneNumber, writer, query, method);
+                    runQuery(proxy, "", threshold, cityCount, 0, 0, comp, zoneNumber, writer, query, method);
 
                 } else {
                     int cityCount = Integer.parseInt(line[1]);
@@ -218,16 +213,13 @@ public class Client {
                     int maxPopulation = Integer.parseInt(line[3]);
                     String[] zoneLine = line[4].split(":");
                     int zoneNumber = Integer.parseInt(zoneLine[1]);
-
-                    runNumberOfCountriesMM(proxy, cityCount, minPopulation, maxPopulation, zoneNumber, writer, query, method);
+                    runQuery(proxy, "", 0, cityCount, minPopulation, maxPopulation, "", zoneNumber, writer, query, method);
                 }
                 Thread.sleep(T);
             }
 
             for (Thread thread : this.threads) {
-                thread.join(); 
-                //I am doing this to make sure that we wait for all threads to finish and gather their results before doing 
-                // any calculation (avoid making calculation without all of the thread's results)
+                thread.join(); //I am doing this to make sure that we wait for all threads to finish and gather their results before doing any calculation
             }
 
             calculateStatistics(writer);
@@ -238,14 +230,39 @@ public class Client {
         }
     }
 
-    private void runPopulationOfCountry(ProxyInterface proxy, String country, int zone, BufferedWriter writer, String query, String method) {
+
+    private void runQuery(
+        ProxyInterface proxy, 
+        String country,
+        int threshold,
+        int cityCount,
+        int minPopulation,
+        int maxPopulation,
+        String comp,
+        int zone, 
+        BufferedWriter writer, 
+        String query, 
+        String method
+    ) {
         Thread thread = new Thread(() -> { //I am using thread to simulate that a client does not need to wait more than 20 or 50 ms for a earlier client-thread to finish
 
             try {
                 long start = System.currentTimeMillis();
+                String cacheKey;  // creating cache key
+
+                if (method.equals("getPopulationofCountry")) {
+                    cacheKey = method + ":" + country; 
+                } else if (method.equals("getNumberofCities"))  {
+                    cacheKey = method + ":" + country + ":" + threshold + ":" + comp;
+                } else if (method.equals("getNumberofCountries")) {
+                    cacheKey = method + ":" + cityCount + ":" + threshold + ":" + comp;
+                } else {
+                    cacheKey = method + ":" + cityCount + ":" + minPopulation + ":" + maxPopulation;
+                }
 
                 if (clientCache) {
-                    String cacheResult = cache.checkCache(method + country);   // check the cache for the query
+                    String cacheResult = cache.checkCache(cacheKey); // check the cache for the query
+                    System.out.println(cacheResult);
                     if (cacheResult != null) {  // if query is in cache
                         long result = Long.parseLong(cacheResult);
                         long end = System.currentTimeMillis();
@@ -261,14 +278,26 @@ public class Client {
                 String serverName = serverInfo[2];
                 Registry serverRegistry = LocateRegistry.getRegistry(port);
                 ServerInterface serverStub = (ServerInterface) serverRegistry.lookup(serverName);
-                
-                long[] results = serverStub.getPopulationofCountry(country);
+                long[] results;
+
+                if (method.equals("getPopulationofCountry")) {
+                    results = serverStub.getPopulationofCountry(country);
+                } else if (method.equals("getNumberofCities"))  {
+                    results = serverStub.getNumberofCities(country, threshold, comp);
+                } else if (method.equals("getNumberofCountries")) {
+                    results = serverStub.getNumberofCountries(cityCount, threshold, comp);
+                } else {
+                    results = serverStub.getNumberofCountriesMM(cityCount, minPopulation, maxPopulation);
+                }
+
                 long serverResult = results[0];
+
+                if (clientCache) { 
+                    cache.addToCache(cacheKey, Long.toString(serverResult)); 
+                } // add query to cache (if we are using cache)
+
                 long waitingTime = results[1];
                 long executionTime = results[2];
-
-                if (clientCache) { cache.addToCache(method + country, Long.toString(serverResult)); }   // add query to cache (if we are using cache)
-
                 long end = System.currentTimeMillis();
                 long turnaroundTime = end - start;
                 addTaskInfo(method, turnaroundTime, executionTime, waitingTime);
@@ -281,145 +310,7 @@ public class Client {
             }
             
         });
-        threads.add(thread); //recurring for all of the four run-methods, I do this to make sure that each thread (client) is finished with their operation
-        thread.start();
-    }
-
-    private void runNumberOfCities(ProxyInterface proxy, String country, int threshold, String comp, int zone, BufferedWriter writer, String query, String method) {
-        Thread thread = new Thread(() -> {
-
-            try {
-                long start = System.currentTimeMillis();
-
-                if (clientCache) {
-                    String cacheResult = cache.checkCache(method + country + threshold + comp);   // check the cache for the query
-                    if (cacheResult != null) {  // if query is in cache
-                        long result = Long.parseLong(cacheResult);
-                        long end = System.currentTimeMillis();
-                        long turnaroundTime = end - start;
-                        addTaskInfo(method, turnaroundTime, 0, 0);
-                        writeToFile(writer, turnaroundTime, result, 0, 0, query, "client cache");
-                        return;
-                    }
-                }
-                
-                String[] serverInfo = proxy.requestServer(zone);
-                int port = Integer.parseInt(serverInfo[1]);
-                String serverName = serverInfo[2];
-                Registry serverRegistry = LocateRegistry.getRegistry(port);
-                ServerInterface serverStub = (ServerInterface) serverRegistry.lookup(serverName);
-                
-                long[] results = serverStub.getNumberofCities(country, threshold, comp);
-                long serverResult = results[0];
-                long waitingTime = results[1];
-                long executionTime = results[2];
-
-                if (clientCache) { cache.addToCache(method + country + threshold + comp, Long.toString(serverResult)); }   // add query to cache (if we are using cache)
-
-                long end = System.currentTimeMillis();
-                long turnaroundTime = end - start;
-                addTaskInfo(method, turnaroundTime, executionTime, waitingTime);
-                writeToFile(writer, turnaroundTime, serverResult, waitingTime, executionTime, query, serverName);
-
-            } catch (RemoteException | NotBoundException e) {
-                System.err.println("Error");
-            } catch (IOException e) {
-                System.err.println("Error");
-            }
-        });
-        threads.add(thread);
-        thread.start();
-    }
-
-    private void runNumberOfCountries(ProxyInterface proxy, int cityCount, int threshold, String comp, int zone, BufferedWriter writer, String query, String method) {
-        Thread thread = new Thread(() -> {
-
-            try {
-                long start = System.currentTimeMillis();
-
-                if (clientCache) {
-                    String cacheResult = cache.checkCache(method + cityCount + threshold);   // check the cache for the query
-                    if (cacheResult != null) {  // if query is in cache
-                        long result = Long.parseLong(cacheResult);
-                        long end = System.currentTimeMillis();
-                        long turnaroundTime = end - start;
-                        addTaskInfo(method, turnaroundTime, 0, 0);
-                        writeToFile(writer, turnaroundTime, result, 0, 0, query, "client cache");
-                        return;
-                    }
-                }
-                
-                String[] serverInfo = proxy.requestServer(zone);
-                int port = Integer.parseInt(serverInfo[1]);
-                String serverName = serverInfo[2];
-                Registry serverRegistry = LocateRegistry.getRegistry(port);
-                ServerInterface serverStub = (ServerInterface) serverRegistry.lookup(serverName);
-                
-                long[] results = serverStub.getNumberofCountries(cityCount, threshold, comp);
-                long serverResult = results[0];
-                long waitingTime = results[1];
-                long executionTime = results[2];
-
-                if (clientCache) { cache.addToCache(method + cityCount + threshold, Long.toString(serverResult)); }   // add query to cache (if we are using cache)
-
-                long end = System.currentTimeMillis();
-                long turnaroundTime = end - start;
-                addTaskInfo(method, turnaroundTime, executionTime, waitingTime);
-                writeToFile(writer, turnaroundTime, serverResult, waitingTime, executionTime, query, serverName);
-
-            } catch (RemoteException | NotBoundException e) {
-                System.err.println("Error");
-            } catch (IOException e) {
-                System.err.println("Error");
-            }
-        });
-        threads.add(thread);
-        thread.start();
-    }
-
-    private void runNumberOfCountriesMM(ProxyInterface proxy, int cityCount, int minPopulation, int maxPopulation, int zone, BufferedWriter writer, String query, String method) {
-        Thread thread = new Thread(() -> {
-
-            try {
-                long start = System.currentTimeMillis();
-
-                if (clientCache) {
-                    String cacheResult = cache.checkCache(method + cityCount + minPopulation + maxPopulation);   // check the cache for the query
-                    if (cacheResult != null) {  // if query is in cache
-                        long result = Long.parseLong(cacheResult);
-                        long end = System.currentTimeMillis();
-                        long turnaroundTime = end - start;
-                        addTaskInfo(method, turnaroundTime, 0, 0);
-                        writeToFile(writer, turnaroundTime, result, 0, 0, query, "client cache");
-                        return;
-                    }
-                }
-                
-                String[] serverInfo = proxy.requestServer(zone);
-                int port = Integer.parseInt(serverInfo[1]);
-                String serverName = serverInfo[2];
-                Registry serverRegistry = LocateRegistry.getRegistry(port);
-                ServerInterface serverStub = (ServerInterface) serverRegistry.lookup(serverName);
-                
-                long[] results = serverStub.getNumberofCountriesMM(cityCount, minPopulation, maxPopulation);
-                long serverResult = results[0];
-                long waitingTime = results[1];
-                long executionTime = results[2];
-
-                if (clientCache) { cache.addToCache(method + cityCount + minPopulation + maxPopulation, Long.toString(serverResult)); }   // add query to cache (if we are using cache)
-                
-                long end = System.currentTimeMillis();
-                long turnaroundTime = end - start;
-                addTaskInfo(method, turnaroundTime, executionTime, waitingTime);
-                writeToFile(writer, turnaroundTime, serverResult, waitingTime, executionTime, query, serverName);
-
-            } catch (RemoteException | NotBoundException e) {
-                System.err.println("Error");
-            } catch (IOException e) {
-                System.err.println("Error");
-            }
-        });
-        threads.add(thread);
+        threads.add(thread); //run-methods, I do this to make sure that each thread (client) is finished with their operation
         thread.start();
     }
 
@@ -428,7 +319,6 @@ public class Client {
     }
 
     private synchronized void writeToFile(BufferedWriter writer, long turnaroundTime, long serverResult, long waitingTime, long executionTime, String query, String serverName) throws IOException {
-
         writer.write(serverResult + " " + query + " (turnaround time: " + turnaroundTime + " ms, execution time: " + executionTime + " ms, waiting time: " + waitingTime + " ms, processed by " + serverName + ")");
         writer.newLine();
         System.out.println(serverResult + " " + query + " (turnaround time: " + turnaroundTime + " ms, execution time: " + executionTime + " ms, waiting time: " + waitingTime + " ms, processed by " + serverName + ")");
@@ -455,11 +345,9 @@ public class Client {
                     if (task.getTurnaroundTime() < minTurnaroundTime) { //finding the minimum turn-around time
                         minTurnaroundTime = task.getTurnaroundTime();
                     }
-
                     if (task.getTurnaroundTime() > maxTurnaroundTime) { //finding the maximum turn-around time
                         maxTurnaroundTime = task.getTurnaroundTime();
                     }
-
                     count++; //increment the total amount for a method's invocation
                 }
             }
